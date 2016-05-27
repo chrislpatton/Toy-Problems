7faa948fff0a187e1ee37989f8ed364ba30a28ad
chrislpatton <chrislpatton@gmail.com>
2016-05-27 00:31:29 -0500

    added tests completed Async Map

---
+++ AsyncMap/main.js
@@ -0,0 +1,56 @@
+// Async Map
+// NOTE: This challenge requires some kind of asynchronous test implementation, 
+// and these tests are not yet functional.
+
+// Implement the function asyncMap:
+
+// asyncMap has two parameters, an array of asynchronous functions (tasks) 
+// and a callback.
+
+// Each of the tasks takes a separate callback and invokes that callback when 
+// complete.
+
+// The callback passed to asyncMap is then performed on the results of the 
+// callbacks of the tasks.
+
+// The order of these results should be the same as the order of the tasks.
+// It is important to note that this is not the order in which the tasks 
+// return,but the order in which they are passed to asyncMap.
+
+// Once all the callbacks of the tasks are returned, asyncMap should invoke 
+// the callback on the results array.
+
+//Example:
+
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+ ],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
+
+
+var asyncMap = function(tasks, callback){
+  var results = [];
+  var completed = tasks.length;
+  
+  tasks.forEach(function(task, i) {
+    task(function(result) {
+      results[i] = result;
+      if (--completed === 0) {
+        callback(results);
+      }
+    });
+  });
+};  
